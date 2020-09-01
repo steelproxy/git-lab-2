0aa674838b6b594d8dbbd801a880a87e4f7ebb9a
Collin Rodes <cr271620@ohio.edu>
2020-09-01 16:39:30 -0400

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Collin Rodes
+ *        Date: 9/1/20
+ * Description: Program prints "Hello Git!!" and exits.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
